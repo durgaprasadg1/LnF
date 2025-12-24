c49198634bb350a2c3391a201504bc4b71b7c860
Durgaprasad <durgaprasadgirepunje@gmail.com>
2025-12-24 19:23:04 +0530

    Edit User and its route fixed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers";
+import Providers from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
